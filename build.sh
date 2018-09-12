160d891c47b44c275a6595cf88e71e550a164050
taquy.se <taquy.se@gmail.com>
2018-09-12 16:42:07 +0700

    remove credentials of rabbitmq

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd orchestrator && mvn package &&  cd .. \
-cd service-a && mvn package && cd .. \
-cd service-b && mvn package && cd .. \
-cd service-main && mvn package && cd ..
+cd orchestrator && mvn clean package && docker build -t saga-2-orchestrator . && cd .. \
+cd service-a && mvn clean package && docker build -t saga-2-service-a . && cd .. \
+cd service-b && mvn clean package && docker build -t saga-2-service-b . && cd .. \
+cd service-main && mvn clean package && docker build -t saga-2-service-main . && cd ..
