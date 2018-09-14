d38d8d905b5deeace2c98ac12f89c3de965457ee
taquy.se <taquy.se@gmail.com>
2018-09-14 10:57:08 +0700

    update build.sh: declare image name on maven build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd orchestrator && mvn clean package && cd .. && \
-cd service-account && mvn clean package && cd .. && \
-cd service-room && mvn clean package && cd .. && \
-cd service-ticket && mvn clean package && cd ..
+cd orchestrator && mvn clean package && docker build -t saga-2-service-orchestrator . &&cd .. && \
+cd service-account && mvn clean package && docker build -t saga-2-service-account . &&cd .. && \
+cd service-room && mvn clean package && docker build -t saga-2-service-room . &&cd .. && \
+cd service-ticket && mvn clean package && docker build -t saga-2-service-ticket . &&cd ..
