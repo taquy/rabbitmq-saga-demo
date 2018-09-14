ff664a6a1b141a538de3fe7df81436e57064d6da
taquy.se <taquy.se@gmail.com>
2018-09-14 14:44:37 +0700

    orchestrator v3

--- orchestrator/src/main/java/app/broker/Orchestrator.java
+++ orchestrator/src/main/java/app/broker/Orchestrator.java
@@ -1,6 +1,7 @@
 package app.broker;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Stack;
 
@@ -27,10 +28,13 @@ public class Orchestrator {
 		public Process(Stack<Message> doneTasks) {
 			this.doneTasks = doneTasks;
 			this.status = true;
+			this.result = null;
 		}
 
 		@Override
 		public void onSuccess(Message result) {
+			
+			this.result = result;
 
 			if (result.isDone()) {
 				this.doneTasks.push(result);
@@ -70,66 +74,80 @@ public class Orchestrator {
 			tasks.push(task2);
 
 			totalTasks = tasks.size();
-			
-			// parallel processes pool - any task in this process failed will result in a transaction rollback
+
+			// parallel processes pool - any task in this process failed will result in a
+			// transaction rollback
 			List<Process> processes = new ArrayList<Process>();
 
 			// start transaction execution thread
 			while (doneTasks.size() != totalTasks) {
-				
+
 				if (tasks.isEmpty()) {
-					
+
 					if (processes.size() > 0) {
 						
-						// continuously tracking asynchronous processes status, stop until no processes left to track
-						for (Process process : processes) {
-							if (process.status == false) isFailed = true;
-							processes.remove(process);
+						// continuously tracking asynchronous processes status, stop until no processes
+						// left to track
+						
+						Iterator<Process> iterator = processes.iterator();
+						while (iterator.hasNext()) {
+							Process process = iterator.next();
+							
+							if (process.result != null || !process.status) {
+								
+								if (!process.status) {
+									 isFailed = true;
+								}
+								
+								iterator.remove();
+								
+							}
+							
 						}
-						
+
 						continue;
-						
+
 					}
 					// all processes has been done
-						
+
 					// check transaction status, roll-back if failed
 					if (isFailed) {
-	
+
 						while (!doneTasks.isEmpty()) {
 							Message rollbackTask = doneTasks.pop();
 							rollbackTask.setRollbackCommand(rollbackTask.getCommand());
 							producer.sendAsync(rollbackTask);
 						}
-						
+
 						break;
 					}
-				
+
 				} else {
-				
+
 					Message task = tasks.pop();
-	
+
 					if (task.isAsync()) {
-	
+
 						Process process = new Process(doneTasks);
-						
+
 						processes.add(process);
-	
+
 						producer.sendAsync(task).addCallback(process);
-	
+
 					} else {
-	
+
 						Message result = producer.sendSync(task);
-	
+
 						if (result.isDone()) {
 							doneTasks.push(result);
 						} else {
 							isFailed = true;
 							break;
 						}
-	
+
 					}
 					// all tasks have been sent
-					
+
 				}
 
 			}
